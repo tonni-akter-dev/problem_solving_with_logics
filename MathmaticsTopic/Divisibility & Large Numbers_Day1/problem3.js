e2207bc6989ddb707717a52fc2636f5af67e4adc
unknown <tonni-akter-dev>
2023-05-01 19:48:37 +0600

    Day 1

--- MathmaticsTopic/Divisibility & Large Numbers_Day1/problem3.js
+++ MathmaticsTopic/Divisibility & Large Numbers_Day1/problem3.js
@@ -1,1 +1,19 @@
 // Check if a large number is divisible by 6 or not
+function checkBy6(str) {
+  let n = str.length;
+
+  let lastDigit = str[n - 1] - "0";
+  if (lastDigit % 2 == 0) {
+    return true;
+  }
+
+  let digitSum = 0;
+  for (let i = 0; i < n; i++) {
+    digitSum += str[i] - "0";
+    return digitSum % 3 == 0;
+  }
+  //   console.log(lastDigit)
+}
+let str = "1332";
+if (checkBy6(str)) console.log("Yes");
+else console.log("No");
